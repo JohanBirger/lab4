50f336460f45add58c13d1fc52ddf7dbbb7bde55
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2020-07-30 15:08:31 +0200

    Update Assignment4.java

--- Assignment4.java
+++ Assignment4.java
@@ -1,36 +1,37 @@
-import java.util.Scanner;
 
+    import java.util.Scanner;
 
-/**
- *
- */
-public class Assignment4 {
-
-    /**
-     * @param args not used
-     */
-    public static void main(String[] args) {
-        run();
-    }
 
     /**
      *
      */
-    public static void run() {
+    public class delete {
 
-        Scanner sc = new Scanner(System.in);
-        String user_input = "";
-        Boolean quit = false;
+        /**
+         * @param args not used
+         */
+        public static void main(String[] args) {
+            run();
+        }
 
-        ArraysVocabulary ArraysVocabulary = new ArraysVocabulary();
-        String[] swe_words = ArraysVocabulary.getSwe_words;
-        String[] eng_words = ArraysVocabulary.getEng_words;
+        /**
+         *
+         */
+        public static void run() {
+
+            Scanner sc = new Scanner(System.in);
+            String user_input = "";
+            Boolean quit = false;
+
+            VocabularyArray vocabularyArray = new VocabularyArray();
+            String[] swe_words = vocabularyArray.getSwe_words();
+            String[] eng_words = vocabularyArray.getEng_words();
 
 
-        final String ANSI_GREEN = "\u001B[32m";
-        final String ANSI_RESET = "\u001B[0m";
-        final String ANSI_RED = "\u001B[31m";
-        
+            final String ANSI_GREEN = "\u001B[32m";
+            final String ANSI_RESET = "\u001B[0m";
+            final String ANSI_RED = "\u001B[31m";
+
             while (!quit) {
                 introduction();
                 int j = 0;
@@ -46,9 +47,9 @@ public class Assignment4 {
 
                     if (user_input.equalsIgnoreCase("Q")) {
                         System.out.println("Du svarade på totalt " + index + " glosor och hade " + score + " rätt. Välkommen åter!");
-                        quit = true;
+                        quit = true;}
 
-                        if (user_input.equalsIgnoreCase(eng_words[j])) {
+                        else if (user_input.equalsIgnoreCase(eng_words[j])) {
                             index++;
                             score++;
 
@@ -57,6 +58,7 @@ public class Assignment4 {
                                 quit = true;
                             } else {
                                 System.out.println(ANSI_GREEN + "Korrekt!" + ANSI_RESET + score + " rätt av " + index + " ord.");
+
                             }
                         }
                         //blbl
@@ -72,39 +74,44 @@ public class Assignment4 {
 
                         } else {
                             index++;
-                            if (index == eng_word_length) ;
-                            System.out.println(ANSI_RED + "Fel svar!" + ANSI_RESET + " Korrekt svar är " + eng_words);
-                            System.out.println("Du svarade på totalt " + index + " och hade " + score + "rätt. Välkomen åter!");
-                        } else{
-                            System.out.println("Fel svar!. Rätt svar är " + eng_words + ".");
+                            if (index == eng_word_length) {
+                                System.out.println(ANSI_RED + "Fel svar!" + ANSI_RESET + " Korrekt svar är " + eng_words);
+                                System.out.println("Du svarade på totalt " + index + " och hade " + score + "rätt. Välkomen åter!");
+                            } else {
+                                System.out.println("Fel svar!. Rätt svar är " + eng_words + ".");
 
+
+                            }
 
                         }
-
                     }
                 }
+            }
+
+        public static void introduction() {
+            System.out.println("** GLOSÖVNING - ENGELSKA **");
+            System.out.println("Skriv det engelska ordet. Avsluta programmet genom att skriva Q");
+        }
+
+
+    }
+
+       
+
+
+    class VocabularyArray {
+
+        private String[] swe_words = {"Bil","Bok","Mus","Dator","Fönster","Kopp","Penna","Växt","Gitarr","Glasögon"};
+        private String[] eng_words = {"Car","Book","Mouse","Computer","Window","Cup","Pen","Plant","Guitar","Glasses"};
+
+        public String[] getSwe_words() {
+            return swe_words.clone();
+        }
+
+        public String[] getEng_words() {
+            return eng_words.clone();
         }
     }
-
-    private static void introduction() {
-        System.out.println("** GLOSÖVNING - ENGELSKA **");
-        System.out.println("Skriv det engelska ordet. Avsluta programmet genom att skriva Q");
-    }
-}
-     class ArraysVocabulary {
-
-         public String[] getSwe_words;
-         public String[] getEng_words;
-         private String[] swe_words = {"bil", "hus","springa","blå","baka","hoppa","simma","måne","väg","snäll"};
-
-         private String[] eng_words = {"car", "house","run","blue","bake","jump","swim","moon","road","kind"};
-
-         public String [] getSwe_words () {
-             return this.swe_words; }
-
-         public String [] getEng_words () {
-             return this.eng_words; }
-     }
 
 
 
